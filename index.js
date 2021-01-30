6e3a90ea7b2b7d6684be48e0fb90961e0720e219
Tae Prasongpongchai <taepras@gmail.com>
2021-01-30 12:39:24 +0700

    test ask for location

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict";
 const express = require("express");
-const { actionssdk, Permission } = require("actions-on-google");
+const { conversation, actionssdk, Permission } = require("actions-on-google");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 // const app = dialogflow({ debug:true });
-const { conversation, Image } = require("@assistant/conversation");
+// const { conversation, Image } = require("@assistant/conversation");
 
-const app = actionssdk(); //conversation();
+const app = conversation();
 
 // app.handle("AIR_QUALITY", (conv) => {
 //   console.log("incoming AIR_QUALTY invoke ", JSON.stringify(conv));
@@ -19,7 +19,7 @@ const app = actionssdk(); //conversation();
 //   conv.add(new Permission(options));
 // });
 
-app.intent("ASK_AIR_QUALITY", (conv) => {
+app.handle("AIR_QUALITY", (conv) => {
   console.log("incoming AIR_QUALTY invoke ", JSON.stringify(conv));
   conv.add("Hello world!!");
 
