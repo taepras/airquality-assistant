27ef570592035b826bb791da48daf2305045869a
Tae Prasongpongchai <taepras@gmail.com>
2021-01-30 13:00:57 +0700

    test ask for location

--- index.js
+++ index.js
@@ -4,22 +4,11 @@ const { actionssdk, Permission } = require("actions-on-google");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 // const app = dialogflow({ debug:true });
-const { conversation, Image } = require("@assistant/conversation");
+// const { conversation, Image } = require("@assistant/conversation");
 
-const app = conversation();
+const app = actionssdk();
 
-app.handle("AIR_QUALITY", (conv) => {
-  console.log("incoming AIR_QUALTY invoke ", JSON.stringify(conv));
-  conv.add("Hello world!!");
-
-  const options = {
-    context: "location permission",
-    permissions: ["DEVICE_PRECISE_LOCATION"],
-  };
-  conv.add(new Permission(options));
-});
-
-// app.intent("actions.intent.ASK_AIR_QUALITY", (conv) => {
+// app.handle("AIR_QUALITY", (conv) => {
 //   console.log("incoming AIR_QUALTY invoke ", JSON.stringify(conv));
 //   conv.add("Hello world!!");
 
@@ -29,6 +18,28 @@ app.handle("AIR_QUALITY", (conv) => {
 //   };
 //   conv.add(new Permission(options));
 // });
+
+app.intent("actions.intent.MAIN", (conv) => {
+  console.log("incoming AIR_QUALTY invoke ", JSON.stringify(conv, null, 2));
+  conv.add("Hello world!!");
+
+  const options = {
+    context: "location permission",
+    permissions: ["DEVICE_PRECISE_LOCATION"],
+  };
+  conv.ask(new Permission(options));
+});
+
+app.intent("ASK_AIR_QUALITY", (conv) => {
+  console.log("incoming AIR_QUALTY invoke ", JSON.stringify(conv, null, 2));
+  conv.add("Hello world!!");
+
+  const options = {
+    context: "location permission",
+    permissions: ["DEVICE_PRECISE_LOCATION"],
+  };
+  conv.ask(new Permission(options));
+});
 
 const expressApp = express().use(bodyParser.json());
 expressApp.post("/webhook", app);
