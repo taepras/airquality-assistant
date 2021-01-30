e249298c7aa4b438670a0f9d29c184d158d2a5b8
Tae Prasongpongchai <taepras@gmail.com>
2021-01-30 12:54:46 +0700

    test ask for location

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 // const app = dialogflow({ debug:true });
 const { conversation, Image } = require("@assistant/conversation");
 
-const app = actionssdk(); //conversation();
+const app = conversation();
 
 app.handle("AIR_QUALITY", (conv) => {
   console.log("incoming AIR_QUALTY invoke ", JSON.stringify(conv));
@@ -19,16 +19,16 @@ app.handle("AIR_QUALITY", (conv) => {
   conv.add(new Permission(options));
 });
 
-// app.intent("actions.intent.ASK_AIR_QUALITY", (conv) => {
-//   console.log("incoming AIR_QUALTY invoke ", JSON.stringify(conv));
-//   conv.add("Hello world!!");
+app.intent("actions.intent.ASK_AIR_QUALITY", (conv) => {
+  console.log("incoming AIR_QUALTY invoke ", JSON.stringify(conv));
+  conv.add("Hello world!!");
 
-//   const options = {
-//     context: "location permission",
-//     permissions: ["DEVICE_PRECISE_LOCATION"],
-//   };
-//   conv.add(new Permission(options));
-// });
+  const options = {
+    context: "location permission",
+    permissions: ["DEVICE_PRECISE_LOCATION"],
+  };
+  conv.add(new Permission(options));
+});
 
 const expressApp = express().use(bodyParser.json());
 expressApp.post("/webhook", app);
