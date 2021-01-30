b5fefe4796f2bd3e4a943e767e7e019664e64cd4
Tae Prasongpongchai <taepras@gmail.com>
2021-01-30 11:39:35 +0700

    add boilerplate code

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+const express = require('express');
+const { dialogflow } = require('actions-on-google');
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000;
+const app = dialogflow({ debug:true });
+
+app.intent('AIR_QUALITY', (conv)=>{
+    console.log('incoming AIR_QUALTY invoke ', JSON.stringify(conv));
+    conv.ask('Hello world!!');
+});
+
+const expressApp = express().use(bodyParser.json());
+expressApp.post('/webhook',app);
+
+expressApp.listen(port);
